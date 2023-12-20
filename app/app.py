afe16364a2f1cbeac7023e6610ce3f6fdfd8d636
Karthik <karthikdoestech@gmail.com>
2023-12-20 13:20:59 +0530

    1

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, jsonify, send_file
+import uuid
 from html2image import Html2Image
 import os
 
 app = Flask(__name__)
-hti = Html2Image(custom_flags=["--no-sandbox"])
+hti = Html2Image(
+    browser_executable="C:\Program Files\Google\Chrome Dev\Application\chrome.exe",
+    custom_flags=["--no-sandbox", "--disable-gpu"],
+)
 
-# Fixed filename for the screenshot
-fixed_filename = "screenshot.png"
+# Create the 'images' folder if it doesn't exist
+images_folder = os.path.join(os.getcwd(), "images")
+if not os.path.exists(images_folder):
+    os.makedirs(images_folder)
 
 
 @app.route("/screenshot", methods=["POST"])
@@ -15,15 +21,30 @@ def generate_screenshot():
         html = request.form["html"]
         css = request.form["css"]
 
-        # Generate screenshot and save it to the current folder
-        paths = hti.screenshot(html_str=html, css_str=css, save_as=fixed_filename)
+        # Generate a random filename using UUID
+        random_filename = str(uuid.uuid4()) + ".png"
+        absolute_path = os.path.join(images_folder, random_filename)
 
-        absolute_path = os.path.abspath(fixed_filename)
+        # Set the output path for html2image
+        hti.output_path = images_folder
 
-        # Send the image file directly
-        return send_file(absolute_path, mimetype="image/png")
+        # Print debugging information
+        print(f"Attempting to save file to: {absolute_path}")
+
+        # Generate screenshot and save it to the 'images' folder
+        paths = hti.screenshot(html_str=html, css_str=css, save_as=random_filename)
+
+        # Create a download link
+        download_link = f"https://bjjdoc.pythonanywhere.com/download/{random_filename}"
+
+        return jsonify({"download_link": download_link})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route("/download/<filename>")
+def download_screenshot(filename):
+    return send_file(os.path.join(images_folder, filename), as_attachment=True)
 
 
 if __name__ == "__main__":
